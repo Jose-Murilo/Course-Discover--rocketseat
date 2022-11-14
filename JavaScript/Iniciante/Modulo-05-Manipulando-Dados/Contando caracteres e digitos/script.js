6230ded8cbfa33d59058eacc95d698d6f1e68be8
Jose Murilo <murilojucas360@hotmail.com>
2022-11-14 17:41:02 -0300

    add Character and digit count

---
+++ JavaScript/Iniciante/Modulo-05-Manipulando-Dados/Contando caracteres e digitos/script.js
@@ -0,0 +1,9 @@
+// Manipulando String e Números
+
+
+// Contar quantos caracteres tem uma palavra e quantos dígitos tem um número
+let word = "Paralelepipedo"
+console.log(word.length)
+let number = 1234
+console.log(number.length) // Number não recebe lenght, você tem que converter number para String
+console.log(String(number).length)
