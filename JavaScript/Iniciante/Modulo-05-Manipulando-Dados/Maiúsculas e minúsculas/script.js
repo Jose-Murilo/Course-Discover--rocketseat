e342fc078f3d2e1e5c35b8ec8663a797a4086719
Jose Murilo <murilojucas360@hotmail.com>
2022-11-14 18:03:51 -0300

    add case and lower case manipulation

---
+++ JavaScript/Iniciante/Modulo-05-Manipulando-Dados/Maiúsculas e minúsculas/script.js
@@ -0,0 +1,12 @@
+// Manipulação Strings e Números
+
+//Para transformar uma string em letras maiúsculas pode-se usar o método toUpperCase(), e ao contrário, também é possível usar o toLowerCase() para transformar uma string em todas letras minúsculas.
+
+// Transforme letras minúsculas em maiúsculas. Faça o contrario também
+
+let word = "Programar é muito bacana!"
+// Tudo em maiúsculas
+console.log(word.toUpperCase())
+
+// Para tudo minúsculas
+console.log(word.toLowerCase())
