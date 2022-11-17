2a79cb5f5fe3da26ac63453ae131509167a38897
Jose Murilo <murilojucas360@hotmail.com>
2022-11-16 22:16:03 -0300

    add string separation auteration

--- JavaScript/Iniciante/Modulo-05-Manipulando-Dados/Separando strings/script.js
+++ JavaScript/Iniciante/Modulo-05-Manipulando-Dados/Separando strings/script.js
@@ -7,5 +7,5 @@
 
 let phrase = "Eu quero viver o Amor!"
 let myArray = phrase.split(" ")
-let phraseWithUnderscore = phrase.split(" ")
+let phraseWithUnderscore = myArray.join("_")
 console.log(phraseWithUnderscore)
