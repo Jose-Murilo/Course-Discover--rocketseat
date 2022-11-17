87c49ab91b6294f43bc39861017fd7ab677d2426
Jose Murilo <murilojucas360@hotmail.com>
2022-11-16 22:43:07 -0300

    add array elements

---
+++ JavaScript/Iniciante/Modulo-05-Manipulando-Dados/Elementos do Array/script.js
@@ -0,0 +1,12 @@
+// Manipulando Arrays
+
+// Para contar a quantidade de elementos em um array pode-se usar o método length.
+
+// Contar elementos de um array
+console.log(["a", "b", "c"].length)
+
+console.log([
+    "a",
+    {type: "array"},
+    function() {return "alo"}
+][2]())
