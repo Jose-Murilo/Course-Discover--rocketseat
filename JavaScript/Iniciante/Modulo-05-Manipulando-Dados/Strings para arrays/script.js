d9487f232d74910b497236e9513f9e18b76afa8a
Jose Murilo <murilojucas360@hotmail.com>
2022-11-20 20:05:47 -0300

    add auteration String to Array

--- JavaScript/Iniciante/Modulo-05-Manipulando-Dados/Strings para arrays/script.js
+++ JavaScript/Iniciante/Modulo-05-Manipulando-Dados/Strings para arrays/script.js
@@ -1,5 +1,8 @@
 // Manipulando Arrays
 
+// Para transformar uma string em um array, pode-se usar o objeto Array com o método from(), passando como argumento a string a ser transformada em array. Por exemplo console.log(Array.from("developer").
+
+
 // Transformar uma cadeia de caracteres em elementos de um array com o metodo from().
 
 let word = "manipulação"
