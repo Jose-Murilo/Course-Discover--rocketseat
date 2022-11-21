859f7bca6b3dcbb5631515aa218fa6a8bb048343
Jose Murilo <murilojucas360@hotmail.com>
2022-11-20 21:33:03 -0300

    add past expression and study expression new

---
+++ JavaScript/Iniciante/Modulo-06-Expressões_e_operadores/Expressões/new/script.js
@@ -0,0 +1,14 @@
+/*
+    new
+
+    * left-hand-side expression
+    * criar um novo objeto
+*/
+
+let name = new String("Murilo")
+name.surName = "Duarte"
+let age = new Number(16)
+console.log(name, age)
+
+let date = new Date('2020-12-01')
+console.log(date.__proto__)
