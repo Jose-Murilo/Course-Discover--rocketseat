7b8c174124350e7776ea2a1c7cce875bbb20cb8a
Jose Murilo <murilojucas360@hotmail.com>
2022-11-23 12:38:28 -0300

    add logical operators

---
+++ JavaScript/Iniciante/Modulo-06-Expressões_e_operadores/Operadores/Operadores lógicos/script.js
@@ -0,0 +1,16 @@
+// Operadores lógicos (logical operators)
+
+// - 2 valores booleanos, quando verificados,
+// resultará em verdadeiro ou falso
+
+let pao = false
+let queijo = true
+
+// AND &&
+// console.log(pao && queijo) // os dois tem que ser true
+
+// OR ||
+// console.log(pao || queijo) // um verdadeiro ou o outro
+
+// NOT !
+console.log(!pao) // vai negar ex: se estar ligado vou deixar desligado
