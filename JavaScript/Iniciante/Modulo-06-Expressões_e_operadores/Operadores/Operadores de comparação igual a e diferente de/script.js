ffd3e01a5e824b9f6ae5876dd077c83e82ea8f37
Jose Murilo <murilojucas360@hotmail.com>
2022-11-23 12:29:40 -0300

    add comparison operator

--- JavaScript/Iniciante/Modulo-06-Expressões_e_operadores/Operadores/Operadores de comparação igual a e diferente de/script.js
+++ JavaScript/Iniciante/Modulo-06-Expressões_e_operadores/Operadores/Operadores de comparação igual a e diferente de/script.js
@@ -13,3 +13,34 @@ let two = 2
 // console.log( two != 1 )
 // console.log( one != 1 )
 // console.log( one != "1" )
+
+
+// >>>>> Operadores de comparação estritamente igual e estritamente diferente <<<<<
+
+// ===    estritamente igual a
+// console.log( one === 1 )
+// console.log( one === "1" )
+
+
+// ===    estritamente diferente de
+// console.log( two !== 2 )
+// console.log( two !== "2" )
+
+
+
+// >>>>> Operadores de comparação maior e menor (igual) <<<<<
+
+// >      Maior que
+// console.log(one > two)
+
+// >      Maior igual a
+// console.log(one >= 1)
+// console.log(two >= 1)
+
+// >      Menor que
+// console.log(one < two)
+
+// >      Menor igual a
+// console.log(one <= two)
+// console.log(one <= 1)
+// console.log(one <= 0)
