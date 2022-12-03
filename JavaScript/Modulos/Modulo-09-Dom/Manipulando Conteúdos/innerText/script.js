bd686ce847cffd2285707642f765de29207533f0
Jose Murilo <murilojucas360@hotmail.com>
2022-12-02 23:35:42 -0300

    add folder content manipulation

---
+++ JavaScript/Modulos/Modulo-09-Dom/Manipulando Conteúdos/innerText/script.js
@@ -0,0 +1,8 @@
+// Manipulando conteúdo
+// innerText
+
+const element = document.querySelector('h1')
+
+element.innerText += " Olá dev"
+
+// innerText = Trocar o texto interno do seu elemento html
---
+++ JavaScript/Modulos/Modulo-09-Dom/Manipulando Conteúdos/textContent/script.js
@@ -0,0 +1,12 @@
+// Manipulando conteúdo
+// textContent
+
+const element = document.querySelector('h1')
+// const pegar = prompt("Seu nome")
+// element.innerHTML += pegar
+
+
+element.textContent = "Olá dev"
+console.log(element.textContent)
+
+// textContent = Conteúdo de texto
