76f378f9bfb607cc5878f066341be5e53d9e0cfb
Jose Murilo <murilojucas360@hotmail.com>
2022-12-03 14:22:06 -0300

    add study while

---
+++ JavaScript/Modulos/Modulo-08-Estrutura-de-repetição/while/script.js
@@ -0,0 +1,12 @@
+// Estrutura de repetição
+// while = enquanto
+
+let inicializador = 0
+while (inicializador <= 10) {
+    console.log(inicializador)
+
+    inicializador++ // container 
+}
+
+// Quando se usa o while?
+// R: Geralmente quando não se sabe o momento da parada
