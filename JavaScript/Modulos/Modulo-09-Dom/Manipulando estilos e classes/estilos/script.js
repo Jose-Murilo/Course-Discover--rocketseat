58a1f6d121cb168804c71441eee5d72eb6aad692
Jose Murilo <murilojucas360@hotmail.com>
2022-12-03 13:39:46 -0300

    add Manipulating styles

---
+++ JavaScript/Modulos/Modulo-09-Dom/Manipulando estilos e classes/estilos/script.js
@@ -0,0 +1,27 @@
+// Manipulando conteúdo
+// Alterando estilos com style
+
+const body = document.querySelector('body')
+const h1 = document.querySelector('h1')
+const div = document.querySelector('div')
+
+body.style.backgroundColor = '#333'
+body.style.width = '100%'
+body.style.margin = '0'
+body.style.height = '100vh'
+body.style.display = 'flex'
+body.style.flexDirection = 'column'
+body.style.justifyContent = 'center'
+body.style.alignItems = 'center'
+
+h1.style.color = 'white'
+
+div.style.width = '100px'
+div.style.height = '100px'
+div.style.backgroundColor = 'red'
+div.style.borderRadius = '50%'
+
+// >>>>> Anotações <<<<<
+
+// Os estilos com o '-' ficam com a segunda letra Maiusculas ex:
+// backgroundColor = background-color
