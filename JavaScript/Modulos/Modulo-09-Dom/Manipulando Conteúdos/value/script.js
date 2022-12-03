97b17ed0dd96242e77293ff41c6cd147c0ad2b57
Jose Murilo <murilojucas360@hotmail.com>
2022-12-02 23:45:15 -0300

    add value

---
+++ JavaScript/Modulos/Modulo-09-Dom/Manipulando Conteúdos/value/script.js
@@ -0,0 +1,11 @@
+// Manipulando conteúdo
+// value
+
+const element = document.querySelector('input')
+
+// element.value += " Olá dev"
+// element.value = " Olá dev"
+
+console.log(element.value)
+
+// value = Pode reatribuir valor e pode mostrar valor
