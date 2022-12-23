2bce3dbbaac17b42bda9ad9405ac25096d07c4c0
Jose Murilo <murilojucas360@hotmail.com>
2022-12-23 14:02:00 -0300

    add study class-Extra

---
+++ JavaScript/Class-Extra-JS/Desestruturação no javascript/script.js
@@ -0,0 +1,27 @@
+// Desestruturar 
+// Tirar um valor primitivo ou uma estrutura de dados de dentro de outra estrutura
+
+// *Objeto
+
+const pessoa = {
+    name: 'Elton',
+    age: 26
+}
+
+// const { name } = pessoa
+// const { age } = pessoa
+
+// console.log(name);
+// console.log(age);
+// console.log(pessoa);
+
+
+// Renomeando propriedades
+// const { name:nomePessoa } = pessoa
+// const { age:idade } = pessoa
+
+// console.log(nomePessoa);
+// console.log(idade);
+// console.log(pessoa);
+
+//
