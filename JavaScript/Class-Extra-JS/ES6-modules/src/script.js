f92ff46ca0ad10ff16173c3052f3c77f411dbdd8
Jose Murilo <murilojucas360@hotmail.com>
2022-12-24 15:07:41 -0300

    add study es6 modules

---
+++ JavaScript/Class-Extra-JS/ES6-modules/src/script.js
@@ -0,0 +1,8 @@
+// import { sum, sub, mult, div } from './functions.js'
+
+import { operations } from "./functions.js";
+
+console.log(sum(1, 2))
+console.log(sub(1, 2))
+console.log(mult(1, 2))
+console.log(div(1, 2))
