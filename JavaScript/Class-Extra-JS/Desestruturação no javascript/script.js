9fd1473665f02257fb179502c3e1a3602113f347
Jose Murilo <murilojucas360@hotmail.com>
2022-12-26 14:33:40 -0300

    add study Destructuring

--- JavaScript/Class-Extra-JS/Desestruturação no javascript/script.js
+++ JavaScript/Class-Extra-JS/Desestruturação no javascript/script.js
@@ -3,10 +3,10 @@
 
 // *Objeto
 
-const pessoa = {
-    name: 'Elton',
-    age: 26
-}
+// const pessoa = {
+//     name: 'Elton',
+//     age: 26
+// }
 
 // const { name } = pessoa
 // const { age } = pessoa
@@ -25,4 +25,74 @@ const pessoa = {
 // console.log(idade);
 // console.log(pessoa);
 
-//
+
+// >>>>>> Renomeando propriedades (Gerando um erro) <<<<<<
+
+let idade;
+
+const pessoa2 = {
+    name: 'Murilo',
+    age: 16
+};
+
+({ age:idade } = pessoa2)
+const { name:nomePessoa } = pessoa2
+
+console.log(nomePessoa);
+console.log(idade);
+
+// >>>>>> OBS <<<<<<
+
+// Para tirar o erro, basta você tirar a palavra const e colocar toda a expressão dentro de parenteses
+// O erro de inicialização antes do objeto ocorreu por conta do ";"
+
+
+// >>>>>> *Array <<<<<<
+
+// const fruits = ["banana", "pera", "uva"];
+
+// const [banana, pera] = fruits
+// // const [b, p] = fruits
+// const [,b, p] = fruits
+
+// console.log(banana);
+// console.log(pera);
+
+// console.log(b);
+// console.log(p);
+
+// >>>>>> rest (operator) <<<<<<
+
+// const fruits = ["banana", "pera", "uva"];
+
+// const [banana, ...frutas] = fruits
+
+// console.log(banana, frutas);
+
+
+// >>>>>> Functions (desestruturação com obj) <<<<<<
+
+// function liquidificador({banana, ...frutas}) {
+    
+//     console.log(banana);
+//     console.log(frutas);
+// }
+
+// const fruits = {
+//     banana: "banana",
+//     pera: "pera",
+//     uva: "uva"
+// }
+
+// liquidificador(fruits)
+
+
+// >>>>>> Functions (desestruturação com Array) <<<<<<
+
+// function liquidificador([banana, ...rest]) {
+    
+//     console.log(banana);
+//     console.log(rest);
+// }
+
+// liquidificador(['banana', 'melancia', 'maça'])
