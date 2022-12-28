bdc27d198342021ff8aee10feb4d25c071300e5d
Jose Murilo <murilojucas360@hotmail.com>
2022-12-28 18:56:40 -0300

    add study Async/Await

---
+++ JavaScript/Modulos/Modulo-13-JS-Assincrono-and-Promisses/Promisses/Async-Await/Async - Await/script.js
@@ -0,0 +1,41 @@
+// # Async/Await
+
+// - Maneira de escrever promises
+// - Syntactic Sugar
+
+// await = esperar/aguardar uma promessa
+
+let pedido = true;
+const promessa = new Promise((resolve, reject) => {
+
+  if (pedido) {
+      resolve("Ok, deu certo!");
+    } 
+    
+    reject("Não deu ):");
+});
+
+async function start() {
+  try {
+    const promise = await promessa;
+    return console.log(promise);
+  } catch (error) {
+    console.log("Error: " + error);
+  } finally {
+    console.log("Terminado");
+  }
+}
+
+start();
+
+// Eu fazendo!
+async function nameGitHub(name) {
+  const nameUser = name;
+  const url = `https://api.github.com/users/${nameUser}`;
+  const searchName = await fetch(url).then((response) => response.json());
+
+  console.log(searchName.name);
+  console.log(searchName.bio);
+}
+
+nameGitHub("Jose-Murilo");
