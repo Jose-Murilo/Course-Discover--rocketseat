a62e9d9556cef3a17c7230b05ee1a625755dd41e
Jose Murilo <murilojucas360@hotmail.com>
2022-12-27 23:18:14 -0300

    add auteration review

--- JavaScript/Class-Extra-JS/ES6-modules/src/script.js
+++ JavaScript/Class-Extra-JS/ES6-modules/src/script.js
@@ -1,13 +1,13 @@
-import { sum, sub, mult, div } from './functions.js'
+// import { sum, sub, mult, div } from './functions.js'
 
 // import { operations } from "./functions.js";
 
-// import numberRandom from "./numberAleatorio.js" // Com o export default, não precisa colocar as chaves.
+import numberRandom from "./numberAleatorio.js" // Com o export default, não precisa colocar as chaves.
 
-console.log(sum(1, 2))
-console.log(sub(1, 2))
-console.log(mult(1, 2))
-console.log(div(1, 2))
+// console.log(sum(1, 2))
+// console.log(sub(1, 2))
+// console.log(mult(1, 2))
+// console.log(div(1, 2))
 
 // console.log(operations.sum(1, 5))
 // console.log(operations.sub(1, 5))
@@ -15,4 +15,4 @@ console.log(div(1, 2))
 // console.log(operations.div(1, 5))
 
 
-// console.log(numberRandom(2));
+console.log(numberRandom(2));
