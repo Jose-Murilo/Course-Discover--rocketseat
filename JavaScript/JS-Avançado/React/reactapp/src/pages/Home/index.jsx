4686a36c98ea10cb179a23449efc236721470ce9
Jose Murilo <murilojucas360@hotmail.com>
2023-01-05 23:18:54 -0300

    add study Components and props react

--- JavaScript/JS-Avançado/React/reactapp/src/pages/Home/index.jsx
+++ JavaScript/JS-Avançado/React/reactapp/src/pages/Home/index.jsx
@@ -1,11 +1,17 @@
 import './style.css'
 
+import { Card } from '../../components/Card/index'
+
 export function Home() {
   return (
     <div className='container'>
       <h1>Lista de presença</h1>
       <input type="text" placeholder="Digite o nome..." />
       <button type="button">Adicionar</button>
+
+      <Card name='Murilo' time='10:55:25' />
+      <Card name = 'José' time = '10:57:47' />
+      <Card name = 'Ana' time = '10:57:47' />
     </div>
   )
 }
