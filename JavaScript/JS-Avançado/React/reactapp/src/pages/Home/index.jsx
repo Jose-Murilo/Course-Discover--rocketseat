2caa443021c579ba36b62144cd60f72dd61208d6
Jose Murilo <murilojucas360@hotmail.com>
2023-01-09 00:48:37 -0300

    add study-class react useState

--- JavaScript/JS-Avançado/React/reactapp/src/pages/Home/index.jsx
+++ JavaScript/JS-Avançado/React/reactapp/src/pages/Home/index.jsx
@@ -1,22 +1,28 @@
+import React, { useState } from 'react'
 import './style.css'
-import React, { useStates } from 'react'
 
 import { Card } from '../../components/Card/index'
 
-const [count, setCount] = useStates()
-
-
 export function Home() {
+  const [studentName, setStudentName] = useState();
+
   return (
     <div className='container'>
       <h1>Lista de presença</h1>
-      <input type="text" placeholder="Digite o nome..." />
+      <p>Nome:{studentName}</p>
+
+      <input 
+        type="text"
+        placeholder="Digite o nome..."
+        onChange={e => setStudentName(e.target.value)}
+       />
+
+
       <button type="button">Adicionar</button>
 
       <Card name='Murilo' time='10:55:25' />
       <Card name = 'José' time = '10:57:47' />
       <Card name = 'Ana' time = '10:57:47' />
-
     </div>
   )
 }
