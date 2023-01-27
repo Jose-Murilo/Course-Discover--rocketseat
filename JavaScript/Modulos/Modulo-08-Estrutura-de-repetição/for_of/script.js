5902a2670ddc9c1570006471c3f012ea0807d778
Jose Murilo <murilojucas360@hotmail.com>
2023-01-26 23:57:50 -0300

    add comment for of

--- JavaScript/Modulos/Modulo-08-Estrutura-de-repetição/for_of/script.js
+++ JavaScript/Modulos/Modulo-08-Estrutura-de-repetição/for_of/script.js
@@ -3,3 +3,5 @@ const array = ["A", 2, "B"]
 for (const iterator of array) {
     console.log(iterator);
 }
+
+// For of = Ele vai pegar salvar o nome em uma costante e vai interar ao array pegando o valor dele e guardando na variavel
